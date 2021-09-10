8e572680efbf95e97b9f610512986e3995426cab
Justin Hou <jthou@hotmail.com>
2021-09-10 17:36:53 +0800

    themes based on deepin

--- plymouth/yilu_1/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo cp -r $PWD/../yilu_1/ /usr/share/plymouth/themes 
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/yilu_1/yilu_1.plymouth 100
-sudo update-alternatives --config default.plymouth 
-sudo update-initramfs -u
-
